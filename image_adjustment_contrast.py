e9ee33525f0408da4bbac701feef71ea5fcc9865
mrafin <55218099+mrafin@users.noreply.github.com>
2021-10-31 19:24:23 +0700

    Add files via upload

---
+++ image_adjustment_contrast.py
@@ -0,0 +1,42 @@
+import cv2
+import glob
+import numpy as np
+
+# imdir = 'Dataset/seledri/'
+# ext = ['jpg']
+
+# files = []
+# [files.extend(glob.glob(imdir + '*.jpg'))]
+
+# images = [cv2.imread(file)for file in files]
+
+# i = 1
+# for image in images:
+#     im_adj = cv2.addWeighted(image, 1.5, np.zeros(image.shape, image.dtype), 0, -100)
+#     if i<10:
+#         im_name = 'Dataset/seledri_contras/00'+ str(i) + '.jpg'
+#     else : 
+#         im_name = 'Dataset/seledri_contras/0'+ str(i) + '.jpg'
+#     cv2.imwrite(im_name,im_adj)
+
+#     i+=1
+
+# imdir = 'Dataset/jeruk nipis/'
+# ext = ['jpg']
+
+# files = []
+# [files.extend(glob.glob(imdir + '*.jpg'))]
+
+# images = [cv2.imread(file)for file in files]
+
+# i = 1
+# for image in images:
+#     im_adj = cv2.addWeighted(image, 1.5, np.zeros(image.shape, image.dtype), 0, -100)
+#     if i<10:
+#         im_name = 'Dataset/jeruk_nipis_contras/00'+ str(i) + '.jpg'
+#     else : 
+#         im_name = 'Dataset/jeruk_nipis_contras/0'+ str(i) + '.jpg'
+#     cv2.imwrite(im_name,im_adj)
+
+#     i+=1
+
